e6f9cb86780d3ade12448f9c02685cb5244fbf6d
frixit <frixit@yandex.ru>
2025-03-25 11:33:14 +0300

    configure djagno settings

--- store/store/settings.py
+++ store/store/settings.py
@@ -1,21 +1,46 @@
-from pathlib import Path
+import pathlib
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+import decouple
+from django.utils.translation import gettext_lazy as _
 
-SECRET_KEY = "django-insecure-64c&&^&-yv752xpqh@ivnykf0+^lkq4(o_i=2#gv@mpztep!i_"
+BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
 
-DEBUG = True
 
-ALLOWED_HOSTS = []
+SECRET_KEY = decouple.config(
+    "DJANGO_SECRET_KEY",
+    default="SECRET_KEY",
+)
 
+DEBUG = decouple.config(
+    "DJANGO_DEBUG",
+    default=False,
+    cast=bool,
+)
+
+DEFAULT_USER_IS_ACTIVE = decouple.config(
+    "DJANGO_DEFAULT_USER_IS_ACTIVE",
+    default=False,
+    cast=bool,
+)
+
+ALLOWED_HOSTS = decouple.config(
+    "DJANGO_ALLOWED_HOSTS",
+    default="*",
+    cast=decouple.Csv(),
+)
 
 INSTALLED_APPS = [
+    # Custom applications
+    # Native Django applications # noqa: CM001
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.forms",
+    # External applications
+    "sorl.thumbnail",
 ]
 
 MIDDLEWARE = [
@@ -26,28 +51,54 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
 ]
 
+
+if DEBUG:
+    MIDDLEWARE.append(
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
+    )
+    INSTALLED_APPS.append(
+        "debug_toolbar",
+    )
+
+    INTERNAL_IPS = [
+        "127.0.0.1",
+    ]
+
+
 ROOT_URLCONF = "store.urls"
+
+TEMPLATES_DIR = [BASE_DIR / "templates/"]
 
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": TEMPLATES_DIR,
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
+                # Custom applications
+                # Native Django applications # noqa: CM001
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.i18n",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = "store.wsgi.application"
 
+AUTH_USER_MODEL = "users.UserModel"
+
+
+FORM_RENDERER = "django.forms.renderers.TemplatesSetting"
+
+
+WSGI_APPLICATION = "store.wsgi.application"
 
 DATABASES = {
     "default": {
@@ -73,14 +124,41 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ru-ru"
+
+LANGUAGES = [
+    ("ru-ru", _("Russian")),
+    ("en-us", _("English")),
+]
 
 TIME_ZONE = "UTC"
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
+
+LOCALE_PATHS = [BASE_DIR / "locale/"]
+
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [BASE_DIR / "static_dev"]
+
+STATIC_ROOT = BASE_DIR / "static"
+
+
+MEDIA_ROOT = BASE_DIR / "media/"
+
+MEDIA_URL = "media/"
+
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+
+EMAIL_FILE_PATH = BASE_DIR / "send_mail"
+
+SPAM_EMAIL = decouple.config("DJANGO_MAIL", default="your_email@ya.ru")
--- store/store/urls.py
+++ store/store/urls.py
@@ -1,6 +1,30 @@
-from django.contrib import admin
-from django.urls import path
+import django.conf
+import django.conf.urls.i18n
+import django.conf.urls.static
+import django.contrib.admin
+import django.urls
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    django.urls.path(
+        "i18n/",
+        django.urls.include(
+            django.conf.urls.i18n,
+        ),
+    ),
+    django.urls.path(
+        "admin/",
+        django.contrib.admin.site.urls,
+    ),
 ]
+
+if django.conf.settings.DEBUG:
+    urlpatterns += (
+        django.urls.path(
+            "__debug__/",
+            django.urls.include("debug_toolbar.urls"),
+        ),
+    )
+    urlpatterns += django.conf.urls.static.static(
+        django.conf.settings.MEDIA_URL,
+        document_root=django.conf.settings.MEDIA_ROOT,
+    )
