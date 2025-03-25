34f1b6090cf9dbb137675cd24a82cc7dcac60084
frixit <frixit@yandex.ru>
2025-03-25 13:57:43 +0300

    add: email normalizer for user model

--- store/apps/users/models.py
+++ store/apps/users/models.py
@@ -2,8 +2,11 @@ import django.contrib.auth.models
 import django.db.models
 from django.utils.translation import gettext_lazy as _
 
+import apps.users.email_normalizer
 
 __all__ = ()
+
+normalizer = apps.users.email_normalizer.EmailNormalizer()
 
 
 class UserModel(django.contrib.auth.models.AbstractUser):
@@ -18,3 +21,14 @@ class UserModel(django.contrib.auth.models.AbstractUser):
     class Meta:
         verbose_name = _("user")
         verbose_name_plural = _("users")
+
+
+@django.dispatch.receiver(
+    django.db.models.signals.post_save,
+    sender=UserModel,
+)
+def create_user_profile(sender, instance, created, **kwargs):
+    if hasattr(instance, "email") and instance.email:
+        instance.email = normalizer.normalize(
+            instance.email,
+        )
