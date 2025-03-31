6c2d592a52996a88db5901c33e6d642b986b39de
frixit <frixit@yandex.ru>
2025-03-31 15:19:07 +0300

    add: fonts, html files, update .gitignore

---
+++ store/static_dev/js/main.js
@@ -0,0 +1,11 @@
+// Импорт зависимостей из npm
+import 'bootstrap/dist/js/bootstrap.min.js'; // Bootstrap JS
+import 'tweakpane'; // Tweakpane
+
+// Импорт модулей из js/modules/
+import './modules/carusel.js';
+import './modules/config.js';
+import './modules/tooltip.js';
+import '../css/main.scss';
+// Ваш дополнительный код, если нужно
+console.log('Main JS loaded');
